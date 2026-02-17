24b2f0e2f71d0227bf586cada26a8c1da89d825a
Tanisha Singh <tanisha180304@gmail.com>
2026-02-17 21:37:14 +0530

    ready for render

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
-require("dotenv").config({ path: "./backend/.env" });
-
 const express = require("express");
-//const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 const cors = require("cors");
 const path = require("path");
+require("dotenv").config({ path: "./backend/.env" });
 
-const app = express();   
+
+const app = express();
 
 
 app.use(cors());
@@ -19,16 +19,14 @@ const imageDetectRoute = require("./routes/imageDetect");
 const rateRoute = require("./routes/rateRecipe");
 const favRoute = require("./routes/favoriteRecipe");
 
+app.use("/recipe", matchRoute);
+app.use("/recipe", addRecipeRoute);
 app.use("/recipe", rateRoute);
 app.use("/recipe", favRoute);
-app.use("/recipe", addRecipeRoute);
-app.use("/recipe", matchRoute);
 app.use("/image-detect", imageDetectRoute);
 
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
-
